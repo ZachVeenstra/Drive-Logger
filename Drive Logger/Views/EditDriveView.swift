b084a96f4c6dff21c666c116a1b465a20ad42a73
Zach Veenstra <zach.veen.gr@gmail.com>
2023-11-30 12:30:03 -0500

    Added TimePicker to EditDriveView.

--- Drive Logger/Views/EditDriveView.swift
+++ Drive Logger/Views/EditDriveView.swift
@@ -13,9 +13,9 @@ struct EditDriveView: View {
     
     @AppStorage("totalSeconds") private var totalSeconds: Int = 0
     
-    @State private var hours: Double = 0
-    @State private var minutes: Double = 0
-    @State private var seconds: Double = 0
+    @State private var hours: Int = 0
+    @State private var minutes: Int = 0
+    @State private var seconds: Int = 0
     @State private var date: Date = Date()
     @State private var name: String = ""
     @State private var distance: String = "0"
@@ -28,63 +28,55 @@ struct EditDriveView: View {
 
     
     var body: some View {
-        
-        ZStack {
-            VStack {
-                Form {
-                    Text("Edit Drive")
-                    Section {
-                        Text("Drive Name")
-                            .font(.title3)
-                        TextField("\(drive.name!)", text: $name)
-                            .onAppear() {
-                                name = drive.name!
-                                date = drive.date!
-                                distance = String(drive.distance)
-                                durationSeconds = drive.duration
-                                hours = Double(durationSeconds / SECONDS_IN_HOUR)
-                                minutes = Double(durationSeconds % SECONDS_IN_HOUR / SECONDS_IN_MINUTE)
-                                seconds = Double(durationSeconds % SECONDS_IN_MINUTE)
-                            }
-                            .accessibilityIdentifier("NameField")
-                    }
-
-                    Section {
-                        Text("Drive Duration")
-                            .font(.title3)
-                        VStack() {
-                            Text("Hours: \(Int(hours))")
-                            Slider(value: $hours, in: 0...50).accessibilityIdentifier("HoursSlider")
-                            Text("Minutes: \(Int(minutes))")
-                            Slider(value: $minutes, in: 0...59).accessibilityIdentifier("MinutesSlider")
-                            Text("Seconds: \(Int(seconds))")
-                            Slider(value: $seconds, in: 0...59).accessibilityIdentifier("SecondsSlider")
-
+        VStack {
+            Form {
+                Section {
+                    Text("Drive Name")
+                        .font(.title3)
+                    TextField("\(drive.name!)", text: $name)
+                        .onAppear() {
+                            name = drive.name!
+                            date = drive.date!
+                            distance = String(drive.distance)
+                            durationSeconds = drive.duration
+                            hours = Int(durationSeconds / SECONDS_IN_HOUR)
+                            minutes = Int(durationSeconds % SECONDS_IN_HOUR / SECONDS_IN_MINUTE)
+                            seconds = Int(durationSeconds % SECONDS_IN_MINUTE)
                         }
-                    }
-                                        
-                    Section {
-                        Text("Drive Distance")
-                            .font(.title3)
-                        TextField("Distance", text: $distance)
-                            .keyboardType(.numberPad)
-                            .accessibilityIdentifier("DistanceField")
-                        
-                    }
-                    Button("Submit") {
-                        //TODO: Add an error if duration isn't a number
-                        
-                        let newDuration: Int32 = (Int32(self.hours) * SECONDS_IN_HOUR) + (Int32(self.minutes) * SECONDS_IN_MINUTE) + Int32(self.seconds)
-                        
-                        // Update the total seconds with the difference of the new duration and the old duration.
-                        self.totalSeconds += Int(newDuration) - Int(durationSeconds)
-                        
-                        DataController().addDrive(drive: drive, name: name, duration: newDuration, distance: Double(distance) ?? 0, context: managedObjContext)
-                        
-                        dismiss()
-                    }
-                    .accessibilityIdentifier("SubmitButton")
+                        .accessibilityIdentifier("NameField")
                 }
+
+                Section {
+                    Text("Drive Duration")
+                        .font(.title3)
+                    HStack {
+                        TimePickerView(title: "hours", range: 0...23, selection: $hours)
+                        TimePickerView(title: "min", range: 0...59, selection: $minutes)
+                        TimePickerView(title: "sec", range: 0...59, selection: $seconds)
+                    }
+                }
+                
+                Section {
+                    Text("Drive Distance")
+                        .font(.title3)
+                    TextField("Distance", text: $distance)
+                        .keyboardType(.numberPad)
+                        .accessibilityIdentifier("DistanceField")
+                }
+                
+                Button("Submit") {
+                    //TODO: Add an error if duration isn't a number
+                    
+                    let newDuration: Int32 = (Int32(self.hours) * SECONDS_IN_HOUR) + (Int32(self.minutes) * SECONDS_IN_MINUTE) + Int32(self.seconds)
+                    
+                    // Update the total seconds with the difference of the new duration and the old duration.
+                    self.totalSeconds += Int(newDuration) - Int(durationSeconds)
+                    
+                    DataController().addDrive(drive: drive, name: name, duration: newDuration, distance: Double(distance) ?? 0, context: managedObjContext)
+                    
+                    dismiss()
+                }
+                .accessibilityIdentifier("SubmitButton")
             }
         }
     }
