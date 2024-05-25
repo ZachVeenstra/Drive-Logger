99eee69d0665cdc82e276d71afae8503c68e4fbe
Zach Veenstra <zach.veen.gr@gmail.com>
2024-05-25 12:22:28 -0400

    Now storing data in app group

--- DriveLogger/DataModel/DataController.swift
+++ DriveLogger/DataModel/DataController.swift
@@ -10,16 +10,44 @@ import CoreData
 import SwiftUI
 
 class DataController: ObservableObject {
-    
+
     static let shared = DataController()
-    
-    let container = NSPersistentContainer(name: "DriveModel")
-    
-    init() {
+
+    static var preview: DataController = {
+        let result = DataController(inMemory: true)
+        let viewContext = result.container.viewContext
+
+        return result
+    }()
+
+    let context = NSManagedObjectContext(.privateQueue)
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "DriveModel")
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
+        }
+        let driveModelURL = URL.storeURL(for: "group.com.zachveenstra.drivelogger", databaseName: "DriveModel")
+        let storeDescription = NSPersistentStoreDescription(url: driveModelURL)
+        container.persistentStoreDescriptions = [storeDescription]
+
         container.loadPersistentStores { desc, error in
             if let error = error {
                 print("Failed to load data \(error.localizedDescription)")
             }
         }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
+
+public extension URL {
+    static func storeURL(for appGroup: String, databaseName: String) -> URL {
+        guard let fileContainer = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup) else {
+            fatalError("Unable to create URL for \(appGroup)")
+        }
+
+        return fileContainer.appendingPathComponent("\(databaseName).sqlite")
     }
 }
